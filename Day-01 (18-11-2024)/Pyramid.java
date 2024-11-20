57439e08179ee4ef8c21d8c3bb27583929197d4f
suraj51444 <surajsamal514@gmail.com>
2024-11-20 12:43:24 +0530

    Update Pyramid.java

--- Day-01 (18-11-2024)/Pyramid.java
+++ Day-01 (18-11-2024)/Pyramid.java
@@ -2,8 +2,8 @@
 public class Pyramid {
 	public static void main(String[] args) {
 		/*
-		 * 					*
-		 * 				   ***
+		 * 		    *
+		 * 		   ***
 		 *                *****
 		 *               *******
 		 *              *********
