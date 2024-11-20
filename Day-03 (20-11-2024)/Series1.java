3ec743a2b3edf37e1bd8545241824b414ec285df
Milan Das <45796126+milandas63@users.noreply.github.com>
2024-11-20 12:05:58 +0530

    Add files via upload

---
+++ Day-03 (20-11-2024)/Series1.java
@@ -0,0 +1,10 @@
+
+public class Series1 {
+	public static void main(String[] args) {
+		int seed = 1;
+		for(int i=1; i<10; i++ ) {
+			System.out.print(seed + " ");
+			seed = seed + i;
+		}
+	}
+}
---
+++ Day-03 (20-11-2024)/Series2.java
@@ -0,0 +1,16 @@
+
+public class Series2 {
+
+	public static void main(String[] args) {
+		int seed = 1;
+		for(int i=1; i<=10; i++) {
+			System.out.print(seed + " ");
+			if(i%2==0) {
+				seed = seed*2-1;
+			} else {
+				seed = seed*2+1;
+			}
+		}
+	}
+
+}
