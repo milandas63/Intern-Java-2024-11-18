e725b5218619a4522a948868219d413a1ead7d8f
Sonalidas28 <sonali273das@gmail.com>
2024-12-03 11:59:29 +0530

    Add files via upload

---
+++ Assignment/Sonali Das/Day-09/SecPrimeNo.java
@@ -0,0 +1,28 @@
+package startup;
+
+public class SecPrimeNo {
+
+	public static void main(String[] args) {
+		int start = 50000;
+		int end = 100000;
+		int count = 0;
+		for(int i=start; i<end; i++) {
+			for(int j=2; j<i; j++) {
+				if(i%j==0) {
+					System.out.print(i+" ");
+					count++;
+					if(count>=10) {
+						System.out.println();
+						count = 0;
+					
+				}
+		}
+		 
+		
+
+	} 
+	
+		}
+
+	}
+}
