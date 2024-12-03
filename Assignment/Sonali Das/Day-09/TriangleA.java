df802d910fe6d2b5088dd4b06e226a7bcc488355
Sonalidas28 <sonali273das@gmail.com>
2024-12-03 12:00:48 +0530

    Add files via upload

---
+++ Assignment/Sonali Das/Day-09/TriangleA.java
@@ -0,0 +1,17 @@
+package startup;
+
+public class TriangleA {
+
+	public static void main(String[] args) {
+		int spaces = 25;
+		for(int i=1; i<=9; i++) {
+			
+		}
+			for(int j=1; j<=spaces; j++) System.out.print(" ");
+			for(int j=1; j<=1; j++) System.out.print(j);
+			for(int j=1-1; j>=1; j--) System.out.print(j);
+			System.out.println();
+			spaces = spaces-1;
+	}
+
+}
