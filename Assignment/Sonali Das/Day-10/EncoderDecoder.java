813161e8c4ca5ae08574efeaa9569f90f082abca
Sonalidas28 <sonali273das@gmail.com>
2024-12-03 21:13:19 +0530

    Add files via upload

---
+++ Assignment/Sonali Das/Day-10/EncoderDecoder.java
@@ -0,0 +1,30 @@
+package startup;
+
+public class EncoderDecoder {
+
+	public static void main(String[] args) {
+		String text = " Attack white house";
+		
+		String A = "a";
+		String t = "u";
+		String t = "v";
+		String a = "d";
+		String c = "i";
+		String k = "p";
+		String w = "w";
+		String h = "i";
+		String i = "k";
+		String t = "w";
+		String e = "i";
+		String h = "h";
+		String o = "p";
+		String u = "w";
+		String s = "v";
+		String e = "i";
+		  System.out.println("String text");
+		    System.out.println("Please enter the text you wish to encode.");
+
+		
+	}
+
+}
