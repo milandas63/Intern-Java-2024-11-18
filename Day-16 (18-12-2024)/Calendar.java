73ee8c0a058cb61946c285f83f8a9844ffa781a2
Milan Das <45796126+milandas63@users.noreply.github.com>
2024-12-18 11:19:27 +0530

    Add files via upload

---
+++ Day-16 (18-12-2024)/Calendar.java
@@ -0,0 +1,14 @@
+package suraj;
+
+import javax.swing.JFrame;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Calendar");
+		f.setBounds(200,200,500,400);
+		f.setVisible(true);
+		
+	}
+
+}
