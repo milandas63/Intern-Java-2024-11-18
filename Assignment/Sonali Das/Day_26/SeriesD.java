39640671e3276aa1a1aa8a9210b1ef3eaf4931c6
Sonalidas28 <sonali273das@gmail.com>
2025-01-10 12:54:10 +0530

    Add files via upload

---
+++ Assignment/Sonali Das/Day_26/SeriesD.java
@@ -0,0 +1,19 @@
+
+public class SeriesD {
+
+	public static void main(String[] args) {
+			int seed = 1;
+			for(int i=1; i<10; i++ ) {
+				System.out.print(seed + " ");
+				if(i%2==0) {
+					seed = seed*2-1;
+				} else {
+					seed = seed*2+1;
+				}
+
+		}
+
+	}
+	}
+
+
